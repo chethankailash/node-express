d8c1d4ed0df958e6411292c2e83087bfaf8a075d
chethankailash <chethankailash3@gmail.com>
2018-08-02 11:40:14 +0530

    added a express middleware for log

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const port = 4000;
 
+
+// middlewares
+//log middleware
+
+app.use((req,res,next)=>{
+	console.log(`${req.method}:${req.url}:${new Date()}`);
+	next();
+})
+
+
+
+let products=[{id:1,name:'marker',price:10},{id:2,name:"pencil",price:5},{id:3,name:"compass",price:15}]
+//route handler methods
 
 app.get('/',function(req,res){
 	res.send('hello world');
 
+})
+
+app.get('/products',(req,res)=>{
+	res.send(products);
 })
 
 
@@ -18,6 +36,6 @@ app.get('/contact',(req,res)=>{
 	res.send('<h2>Contact Details</h2>');
 })
 
-app.listen(3000,function(){
-	console.log('Listening on port 3000');
+app.listen(port,function(){
+	console.log('Listening on port 4000');
 })
